acb9db3925ac691597dfb4d3873630971f676a58
Shrikant Kale <58430952+kaleshrikant@users.noreply.github.com>
2025-08-07 19:52:31 +0530

    Initial commit

---
+++ SpringBoot-k8s-Yaml/src/main/java/com/kaleshrikant/SpringBootK8sYamlApplication.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootK8sYamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootK8sYamlApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-k8s-Yaml/src/test/java/com/kaleshrikant/SpringBootK8sYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootK8sYamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
