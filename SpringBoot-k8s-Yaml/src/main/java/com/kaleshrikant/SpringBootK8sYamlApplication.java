99b14c162f143b83a4da8130b1c6bf69a6afda98
Shrikant Kale <58430952+kaleshrikant@users.noreply.github.com>
2025-08-07 21:41:10 +0530

    updated REST-API point

--- SpringBoot-k8s-Yaml/src/main/java/com/kaleshrikant/SpringBootK8sYamlApplication.java
+++ SpringBoot-k8s-Yaml/src/main/java/com/kaleshrikant/SpringBootK8sYamlApplication.java
@@ -2,12 +2,20 @@ package com.kaleshrikant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootK8sYamlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootK8sYamlApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String displayMessage() {
+		return  "Congratulation you have successfully deployed your application to Kubernetes ..!";
 	}
 
 }
