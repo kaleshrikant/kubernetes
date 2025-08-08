ecc3f26359e2052f0f878b9b03250761ee9779f3
Shrikant Kale <58430952+kaleshrikant@users.noreply.github.com>
2025-08-08 08:26:50 +0530

    Initial Commit

---
+++ spring-boot-crud-k8s-example/src/main/java/com/kaleshrikant/SpringBootCrudK8sExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudK8sExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudK8sExampleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-crud-k8s-example/src/test/java/com/kaleshrikant/SpringBootCrudK8sExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudK8sExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
